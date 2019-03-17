0a035e0521982d184fa80cbdb726dd0e663720f0
Martin <1135485226@qq.com>
2019-03-17 20:07:58 +0800

    scale the pooling outputs before element-wise additions

--- python/fcn.py
+++ python/fcn.py
@@ -113,6 +113,43 @@ class FCN8s(nn.Module):
 
         return score  # size=(N, n_class, x.H/1, x.W/1)
 
+class FCN8sScaled(nn.Module):
+
+    def __init__(self, pretrained_net, n_class):
+        super().__init__()
+        self.n_class = n_class
+        self.pretrained_net = pretrained_net
+        self.relu    = nn.ReLU(inplace=True)
+        self.deconv1 = nn.ConvTranspose2d(512, 512, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn1     = nn.BatchNorm2d(512)
+        self.deconv2 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn2     = nn.BatchNorm2d(256)
+        self.deconv3 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn3     = nn.BatchNorm2d(128)
+        self.deconv4 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn4     = nn.BatchNorm2d(64)
+        self.deconv5 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn5     = nn.BatchNorm2d(32)
+        self.classifier = nn.Conv2d(32, n_class, kernel_size=1)
+
+    def forward(self, x):
+        output = self.pretrained_net(x)
+        x5 = output['x5']  # size=(N, 512, x.H/32, x.W/32)
+        x4 = output['x4']  # size=(N, 512, x.H/16, x.W/16)
+        x3 = output['x3']  # size=(N, 256, x.H/8,  x.W/8)
+
+        # x4 and x3 are scaled by the factor of 0.01 and 0.0001 respectively
+        score = self.relu(self.deconv1(x5))               # size=(N, 512, x.H/16, x.W/16)
+        score = self.bn1(score + x4 * 0.01)               # element-wise add, size=(N, 512, x.H/16, x.W/16)
+        score = self.relu(self.deconv2(score))            # size=(N, 256, x.H/8, x.W/8)
+        score = self.bn2(score + x3 * 0.0001)             # element-wise add, size=(N, 256, x.H/8, x.W/8)
+        score = self.bn3(self.relu(self.deconv3(score)))  # size=(N, 128, x.H/4, x.W/4)
+        score = self.bn4(self.relu(self.deconv4(score)))  # size=(N, 64, x.H/2, x.W/2)
+        score = self.bn5(self.relu(self.deconv5(score)))  # size=(N, 32, x.H, x.W)
+        score = self.classifier(score)                    # size=(N, n_class, x.H/1, x.W/1)
+
+        return score  # size=(N, n_class, x.H/1, x.W/1)
+
 class FCN8s_bilinear(nn.Module):
     def __init__(self, pretrained_net, n_class):
         super().__init__()
@@ -129,11 +166,12 @@ class FCN8s_bilinear(nn.Module):
         x4 = output['x4']  # size=(N, 512, x.H/16, x.W/16)
         x3 = output['x3']  # size=(N, 256, x.H/8,  x.W/8)
 
+        # x4 and x3 are scaled by the factor of 0.01 and 0.0001 respectively
         score = nn.functional.interpolate(x5, scale_factor=2, mode='bilinear', align_corners=True)
-        score += x4
+        score += x4 * 0.01
         score = nn.functional.interpolate(score, scale_factor=2, mode='bilinear', align_corners=True)
         score = self.relu(self.conv1(score))
-        score += x3
+        score += x3 * 0.0001
         score = nn.functional.interpolate(score, scale_factor=8, mode='bilinear', align_corners=True)
         score = self.relu(self.conv2(score))
 
@@ -281,10 +319,10 @@ if __name__ == "__main__":
     print("Pass size check")
 
     # test a random batch, loss should decrease
-    fcn_model = FCN8s(pretrained_net=vgg_model, n_class=n_class)
+    fcn_model = FCN8s_bilinear(pretrained_net=vgg_model, n_class=n_class)
     criterion = nn.BCELoss()
     # optimizer = optim.SGD(fcn_model.parameters(), lr=1e-3, momentum=0.9)
-    optimizer = optim.Adam(fcn_model.parameters(), lr=1e-3)
+    optimizer = optim.Adam(fcn_model.parameters(), lr=1e-4)
     input = torch.autograd.Variable(torch.randn(batch_size, 3, h, w))
     y = torch.autograd.Variable(torch.randn(batch_size, n_class, h, w), requires_grad=False)
     for iter in range(10):
